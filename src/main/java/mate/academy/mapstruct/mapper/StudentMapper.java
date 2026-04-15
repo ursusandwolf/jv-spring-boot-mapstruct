db5c6466ae9f0aaddc7fea9b47d1987829a2740c
alex <ursusandwolf@protonmail.com>
2026-04-15 16:22:35 +0300

    setSubjects

--- src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
@@ -1,19 +1,34 @@
 package mate.academy.mapstruct.mapper;
 
+import java.util.ArrayList;
+import java.util.List;
 import mate.academy.mapstruct.dto.student.CreateStudentRequestDto;
 import mate.academy.mapstruct.dto.student.StudentDto;
 import mate.academy.mapstruct.dto.student.StudentWithoutSubjectsDto;
 import mate.academy.mapstruct.model.Student;
+import mate.academy.mapstruct.model.Subject;
 import org.mapstruct.Mapper;
 import org.mapstruct.MapperConfig;
 import org.mapstruct.Mapping;
 
 @Mapper(config = MapperConfig.class, uses = GroupMapper.class)
 public interface StudentMapper {
+    @Mapping(source = "group.id", target = "groupId")
     StudentDto toDto(Student student);
 
+    @Mapping(source = "group.id", target = "groupId")
     StudentWithoutSubjectsDto toStudentWithoutSubjectsDto(Student student);
 
     @Mapping(target = "group", source = "groupId", qualifiedByName = "groupById")
+    @Mapping(target = "subjects", source = "subjects", qualifiedByName = "subjectById")
     Student toModel(CreateStudentRequestDto requestDto);
+
+    default void setSubjects(List<Long> subjectsId, Student student){
+        ArrayList<Subject> subjects = new ArrayList<>(subjectsId.size());
+        for (Long id : subjectsId) {
+            Subject subject = new Subject(id);
+            subjects.add(subject);
+        }
+        student.setSubjects(subjects);
+    }
 }
